63af2e0e5b758e09af52eef62b57b77c01e8ff67
The Wizard Bear <42446683+TheWizardBear@users.noreply.github.com>
2020-11-09 18:20:01 +0000

    added -V and --version to the CLI tool

--- cli.ts
+++ cli.ts
@@ -1,5 +1,6 @@
 import { imageSettings, printImageString } from "./mod.ts";
 import { colors, parse } from "./deps.ts";
+import version from "./version.ts";
 
 const parsedArgs = parse(Deno.args);
 
@@ -7,28 +8,37 @@ const textImageSettings: imageSettings = {
   path: parsedArgs.file ?? parsedArgs.f,
 };
 
-if (parsedArgs["character-map"]) {
-  textImageSettings.characterMap = String(parsedArgs["character-map"]);
-}
-if (parsedArgs.m) textImageSettings.characterMap = String(parsedArgs.m);
+if(typeof textImageSettings.path != "undefined"){
 
-if (parsedArgs.width) textImageSettings.width = parseInt(parsedArgs.width);
-if (parsedArgs.w) textImageSettings.width = parseInt(parsedArgs.w);
+  if (parsedArgs["character-map"]) {
+    textImageSettings.characterMap = String(parsedArgs["character-map"]);
+  }
+  if (parsedArgs.m) textImageSettings.characterMap = String(parsedArgs.m);
 
-if (parsedArgs.inverted !== undefined) {
-  textImageSettings.inverted =
-    !(parsedArgs.inverted === "false" || !parsedArgs.inverted);
-}
-if (parsedArgs.i !== undefined) {
-  textImageSettings.inverted = !(parsedArgs.i === "false" || !parsedArgs.i);
-}
+  if (parsedArgs.width) textImageSettings.width = parseInt(parsedArgs.width);
+  if (parsedArgs.w) textImageSettings.width = parseInt(parsedArgs.w);
 
-if (parsedArgs.color !== undefined) {
-  textImageSettings.color =
-    !(parsedArgs.color === "false" || !parsedArgs.color);
-}
-if (parsedArgs.c !== undefined) {
-  textImageSettings.color = !(parsedArgs.c === "false" || !parsedArgs.c);
-}
+  if (parsedArgs.inverted !== undefined) {
+    textImageSettings.inverted =
+      !(parsedArgs.inverted === "false" || !parsedArgs.inverted);
+  }
+  if (parsedArgs.i !== undefined) {
+    textImageSettings.inverted = !(parsedArgs.i === "false" || !parsedArgs.i);
+  }
 
-await printImageString(textImageSettings);
+  if (parsedArgs.color !== undefined) {
+    textImageSettings.color =
+      !(parsedArgs.color === "false" || !parsedArgs.color);
+  }
+  if (parsedArgs.c !== undefined) {
+    textImageSettings.color = !(parsedArgs.c === "false" || !parsedArgs.c);
+  }
+
+  await printImageString(textImageSettings);
+
+}else if (parsedArgs.V){
+  console.log(`terminal_images ${version}`)
+}else if (parsedArgs.version){
+  console.log(`terminal_images ${version}\ndeno ${Deno.version.deno}`)
+  
+}
