2cce7bccb82f312300886bd51231e1399bf17e18
The Wizard Bear <42446683+TheWizardBear@users.noreply.github.com>
2020-11-09 17:37:32 +0000

    fixed unpng version

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export { decode as decodeJpeg } from "https://deno.land/x/jpegts@1.1/mod.ts";
 export { parse } from "https://deno.land/std@0.77.0/flags/mod.ts";
 export * as colors from "https://deno.land/std@0.77.0/fmt/colors.ts";
-import unpng from "https://cdn.skypack.dev/upng-js@v2.2.0";
+import unpng from "https://cdn.skypack.dev/upng-js@v2.1.0";
 export const decodePng = (unpng as any).decode;
